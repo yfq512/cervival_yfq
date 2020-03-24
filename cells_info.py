4ad315c20d15d20b5f066affc27ba43368d72033
yfq512 <1085561337@qq.com>
2020-03-24 15:26:17 +0800

    修改bug

--- cells_info.py
+++ cells_info.py
@@ -16,6 +16,8 @@ def get_cell_nuclei_mask(img, img_org, value):
     contours_best = []
     area_max = 0
     _long = 0
+    if len(contours) == 0:
+        return 0, 0, 0, 0, 0, 0, 0
     for cnt in range(0, len(contours)):
         x,y,w,h=cv2.boundingRect(contours[cnt])
         xo2 = x+w/2
@@ -26,16 +28,20 @@ def get_cell_nuclei_mask(img, img_org, value):
             long_ = _long
     area = cv2.contourArea(contours_best)
     perimeter = cv2.arcLength(contours_best,True)
+    if area == 0:
+        return 0, 0, 0, 0, 0, 0, 0
     rule = perimeter/area
     thresh_ = thresh*0
     thresh__ = cv2.fillConvexPoly(thresh_, contours_best, 255)
     value_1,_ = bf.get_2value(img_org,chan=2, mask = thresh__)
-    return thresh__, cnt, area, perimeter, rule, value_1
+    return 1, thresh__, cnt, area, perimeter, rule, value_1
     
     
 def get_cell_cytoplasm_mask(img, nuclei_mask, img_org, value):
     ret_, thresh = cv2.threshold(img, value, 255, cv2.THRESH_BINARY_INV)
     image_, contours, hierarchy_ = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) == 0:
+        return 0,0,0,0,0,0
     area_ = 0
     contours_max = []
     for cnt in range(0, len(contours)):
@@ -44,13 +50,15 @@ def get_cell_cytoplasm_mask(img, nuclei_mask, img_org, value):
             contours_max = contours[cnt]
             area_ = area
     perimeter = cv2.arcLength(contours_max,True)
+    if area_ == 0:
+        return 0,0,0,0,0,0
     rule = perimeter/area_
     thresh_ = thresh*0
     thresh__ = cv2.fillConvexPoly(thresh_, contours_max, 255)
     nuclei_mask_off = np.ones(nuclei_mask.shape)*255 - nuclei_mask
     thresh___ = thresh__*(nuclei_mask_off/255)
     value_1,_ = bf.get_2value(img_org,chan = 0,mask = thresh__)
-    return thresh___, area_, perimeter, rule, value_1
+    return 1, thresh___, area_, perimeter, rule, value_1
  
 if __name__ == "__main__":
     dstroot = 'crop'
@@ -63,8 +71,12 @@ if __name__ == "__main__":
         img_gray = cv2.imread(cellpath, 0)
         img_gray = bf.get_fit_img(img_gray)
         value_1,value_2 = bf.get_2value(img_gray)
-        cell_nuclei_mask, nuclei_cnt, nuclei_area, nuclei_circ, nuclei_rule, cell_nuclei_value = get_cell_nuclei_mask(img_gray, img, value_1) #获取细胞核掩码、个数、面积、周长、核形规则度、获取细胞核深染程度
-        cell_cytoplasm_mask, cytoplasm_area, cytoplasm_circ, cytoplasm_rule, cell_cytoplasm_value = get_cell_cytoplasm_mask(img_gray, cell_nuclei_mask, img, value_2) #获取细胞质掩码、面积、周长、细胞规则度、获取细胞质情况
+        sign_nuclei, cell_nuclei_mask, nuclei_cnt, nuclei_area, nuclei_circ, nuclei_rule, cell_nuclei_value = get_cell_nuclei_mask(img_gray, img, value_1) #获取细胞核掩码、个数、面积、周长、核形规则度、获取细胞核深染程度
+        if sign_nuclei == 0:
+            continue
+        sign_cytoplasm, cell_cytoplasm_mask, cytoplasm_area, cytoplasm_circ, cytoplasm_rule, cell_cytoplasm_value = get_cell_cytoplasm_mask(img_gray, cell_nuclei_mask, img, value_2) #获取细胞质掩码、面积、周长、细胞规则度、获取细胞质情况
+        if sign_cytoplasm == 0:
+            continue
         cell_N_C = nuclei_area/cytoplasm_area #计算核质比
         cellinfo_keys = ['cellpath','nuclei_cnt','nuclei_area','nuclei_circ','nuclei_rule','cell_nuclei_value','cytoplasm_area','cytoplasm_circ','cytoplasm_rule','cell_cytoplasm_value','cell_N_C']
         cellinfo_values = [cellpath,nuclei_cnt,nuclei_area,nuclei_circ,nuclei_rule,cell_nuclei_value,cytoplasm_area,cytoplasm_circ,cytoplasm_rule,cell_cytoplasm_value,cell_N_C]
--- main_end.py
+++ main_end.py
@@ -1,4 +1,4 @@
-import os
+import os, copy
 import numpy as np
 import shutil 
 
@@ -53,30 +53,30 @@ def get_sign(cells_info_file): #解析细胞信息
     cytoplasm_value_ = np.array(cytoplasm_value_)
     cell_N_C_ = np.array(cell_N_C_)
     
-    nuclei_area_abnormal = find_abnormal_data(nuclei_area_, 0.2) #单项阈值
-    nuclei_rule_abnormal = find_abnormal_data(nuclei_rule_, 0.2)
-    nuclei_value_abnormal = find_abnormal_data(nuclei_value_, 0.2)
-    cytoplasm_area_abnormal = find_abnormal_data(cytoplasm_area_, 0.2)
-    cytoplasm_rule_abnormal = find_abnormal_data(cytoplasm_rule_, 0.2)
-    cytoplasm_value_abnormal = find_abnormal_data(cytoplasm_value_, 0.2)
-    cell_N_C_abnormal = find_abnormal_data(cell_N_C_, 0.2)
+    nuclei_area_abnormal = find_abnormal_data(nuclei_area_, 2) #单项阈值
+    nuclei_rule_abnormal = find_abnormal_data(nuclei_rule_, 0.5)
+    nuclei_value_abnormal = find_abnormal_data(nuclei_value_, -0.2)
+    cytoplasm_area_abnormal = find_abnormal_data(cytoplasm_area_, 0.1)
+    cytoplasm_rule_abnormal = find_abnormal_data(cytoplasm_rule_, 0.1)
+    cytoplasm_value_abnormal = find_abnormal_data(cytoplasm_value_, -0.2)
+    cell_N_C_abnormal = find_abnormal_data(cell_N_C_, 4)
 
     cells_infos = []
     cells_infos = [nuclei_area_abnormal, nuclei_rule_abnormal, nuclei_value_abnormal, cytoplasm_area_abnormal, cytoplasm_rule_abnormal, cytoplasm_value_abnormal, cell_N_C_abnormal]
     cells_infos = np.array(cells_infos)
-    cells_infos_2 = cells_infos
-    cells_infos_2[0,:] = cells_infos[0,:] * 0.2 #综合权重
-    cells_infos_2[1,:] = cells_infos[1,:] * 0.05
-    cells_infos_2[2,:] = cells_infos[2,:] * 0.2
-    cells_infos_2[3,:] = cells_infos[3,:] * 0.05
-    cells_infos_2[4,:] = cells_infos[4,:] * 0.05
-    cells_infos_2[5,:] = cells_infos[5,:] * 0.1
-    cells_infos_2[6,:] = cells_infos[6,:] * 0.35
+    cells_infos_2 = cells_infos.copy()
+    cells_infos_2[0,:] = cells_infos[0,:] * 20 #综合权重
+    cells_infos_2[1,:] = cells_infos[1,:] * 5
+    cells_infos_2[2,:] = cells_infos[2,:] * 20
+    cells_infos_2[3,:] = cells_infos[3,:] * 5
+    cells_infos_2[4,:] = cells_infos[4,:] * 5
+    cells_infos_2[5,:] = cells_infos[5,:] * 10
+    cells_infos_2[6,:] = cells_infos[6,:] * 35
     cells_infos_2 = np.array(cells_infos_2)
     cnt_2 = 0
     for n in range(0, cells_infos_2.shape[1]):
-        sum_ = cells_infos_2[:,n].sum()
-        if sum_ >= 0:
+        sum_ = sum(cells_infos_2[:,n])
+        if sum_ >= 40:
             cellpath = cells_info[n]['cellpath']
             newpath = os.path.join('cells_abnormal', cellpath.split('/')[1])
             shutil.copy(cellpath, newpath)
