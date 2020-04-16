498599c200c303724cb953ef723025b9c68fb390
yfq512 <1085561337@qq.com>
2020-04-16 01:59:40 +0000

    加入批量处理数据方法

--- main.py
+++ main.py
@@ -35,6 +35,47 @@ def get_abnormal_cells():
     print('================find abnormal cells================')
     find_normal_cells.main2()
 
+def main2():
+    t1 = time.time()
+    dst = ['crop','clusters','ab_clusters','valid','invalid','cells_info','ab_cells']
+    clean_file(dst)
+    fovs_root = 'fovs'
+    list_fovs = os.listdir(fovs_root)
+    if len(list_fovs) == 0:
+        print('================can not find files in fovs================')
+        exit()
+    seg_cells(fovs_root)
+
+    crop_root = 'crop'
+    list_crop = os.listdir(crop_root)
+    if len(list_crop) == 0:
+        print('================can not find files in crop================')
+        exit()
+    get_valid_cells_info()
+
+    clusters_info__root = 'clusters'
+    list_clusters_info = os.listdir(clusters_info__root)
+    if len(list_clusters_info) == 0:
+        print('================can not find files in clusters================')
+        ab_clusters_cnt = 0
+    else:
+        get_ab_clusters()
+        ab_clusters_cnt = len(os.listdir('ab_clusters'))
+
+    cells_info__root = 'cells_info'
+    list_cells_info = os.listdir(cells_info__root)
+    if len(list_cells_info) == 0:
+        print('================can not find files in cells_info================')
+        valid_cells_cnt = 0
+    else:
+        valid_cells_cnt = len(os.listdir('valid'))
+    get_abnormal_cells()
+    ab_cells_cnt = len(os.listdir('ab_cells'))
+    print('valid cells cnt:', valid_cells_cnt)
+    print('abnormal cells cnt:', ab_cells_cnt)
+    print('abnormal clusters cnt:', ab_clusters_cnt)
+    print('time cost',time.time() - t1)
+
 if __name__ == "__main__":
     t1 = time.time()
     dst = ['crop','clusters','ab_clusters','valid','invalid','cells_info','ab_cells']
